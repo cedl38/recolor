b6810d1d5b43cfbf619eae42dc1f32c06b99e250
cedric <cedl38@gmail.com>
2013-03-19 17:50:49 +0100

    add recursive functionality, renamed default_color_sheme* to color_sheme_ini*

--- recolor.sh
+++ recolor.sh
@@ -49,7 +49,7 @@ convert $1 -define modulate:colorspace=HSB -modulate $modulate_brighness,$modula
 substitute_color() {
 image=$1
 j=1;
-set $default_color_scheme
+set $color_scheme_ini
 	for i
 	do
 	sed -i "s/\#$i/\#$color_scheme[j]/g" $image
@@ -66,7 +66,7 @@ colors=($(grep -o '#[0-9a-ZA-Z]*' $1 | cut -d'#' -f2))
 set $colors
 for i
 do
-echo ${i:0:2}${i:4:2}${i:8:2}
+echo ${i:0:2}${i:4:2}${i:8:2} | tr '[A-Z]' '[a-z]'
 done
 }
 
@@ -121,8 +121,10 @@ ARGUMENTS :
 
 # default recolor function for png files
 recolor_png=recolor
-path_lists='dir'
+color_scheme_ini_xpm='color_scheme_ini.xpm'
+paths_lists=''
 composite='FALSE'
+
 while getopts b:c:Cf:FGh:Op:s: opt
 do
 	case $opt in
@@ -133,7 +135,8 @@ do
 	G)	arg=G; recolor_png=discolor	;;
 	h)	arg=h; modulate_hue=$OPTARG ;;
 	O)	composite='TRUE' ;;
-	p)	path_lists='file'; source $OPTARG ;;
+	p)	source $OPTARG; paths_lists="$OPTARG"
+		cp $paths_lists paths_lists ;;
 	s)	modulate_saturation=$OPTARG ;;
 	\?)	show_help; exit 1 ;;
 	esac
@@ -150,6 +153,21 @@ shift $(($OPTIND -1))
 
 if [[ $2 == '' ]]
 then
+	# recursive functionality
+	if [[ $paths_lists == '' ]]
+	then
+		if [[ -f color_scheme.xpm ]]
+		then
+			# load generated color_scheme.xpm if exist
+			color_scheme_ini_xpm='color_scheme.xpm'
+		fi
+		if [[ -f paths_lists ]]
+		then
+			# load generated paths_lists if exist
+			paths_lists='paths_lists'
+			source paths_lists
+		fi
+	fi
 	if [[ $1 == '' ]]
 	then
 	IMAGE_DIR_OUT=$MAIN_DIR/cache
@@ -174,9 +192,9 @@ c|f|F|G|h)
 	# top color				#729fcf
 	# buttom color			#6194cb
 	# border buttom color	#3465a4
-	default_color_scheme=($(grep -o '#[1-9a-ZA-Z]*' default_color_scheme.xpm | cut -d'#' -f2))
-	set $default_color_scheme
-	echo "default color scheme : $default_color_scheme"
+	color_scheme_ini=($(grep -o '#[1-9a-ZA-Z]*' $color_scheme_ini_xpm | cut -d'#' -f2))
+	set $color_scheme_ini
+	echo "default color scheme : $color_scheme_ini"
 	top_color=$1; buttom_color=$2; border_color=$3
 esac
 
@@ -187,25 +205,26 @@ esac
 echo $modulate_hue
 case $arg in
 c|f|F|G|h)
-	cp default_color_scheme.xpm color_scheme.xpm
+	cp $color_scheme_ini_xpm color_scheme.xpm
 	#recolor_xpm color_scheme.xpm
 	color_scheme=($(recolor_xpm color_scheme.xpm))
 	echo "new color scheme : $color_scheme"
+	cp $color_scheme_ini_xpm color_scheme.xpm
+	substitute_color color_scheme.xpm
 esac
 
 cd $IMAGE_DIR_OUT
 case $arg in
 c|f|F|G|h)
 echo "convert images : $SUBDIRS..."
-	if [[ $path_lists == 'file' ]]
+	if [[ $paths_lists == '' ]]
 	then
-		recolor_paths=$RECOLOR_PATHS
-		png_recolor_paths=($(make_paths $png_subdirs))
-		svg_recolor_paths=($(make_paths $svg_subdirs | sed 's/.png/.svg/g'))
-	else
-	#elif [[ $path_lists == 'dir' ]]
 		svg_recolor_paths=($(find **/*.svg -type f))
 		png_recolor_paths=($(find **/*.png -type f))
+	else
+		recolor_paths=$RECOLOR_PATHS
+		png_recolor_paths=($(make_paths $png_subdirs))
+		svg_recolor_paths=($(make_paths $svg_subdirs | sed 's/.png/.svg/g')) 
 	fi
 
 	recolor_path $recolor_png $png_recolor_paths
@@ -214,8 +233,16 @@ esac
 
 if [[ $composite == 'TRUE' ]]
 then
-	if [[ $path_lists == 'file' ]]
+	if [[ $paths_lists == '' ]]
 	then
+	echo "compose images : $IMAGE_DIR_OUT..."
+	part1_paths=($(find **/*.png -type f))
+	composite_paths=$IMAGE_DIR_OUT
+	part2_dir=stock
+	cd $part2_dir
+	part2_paths=($(find **/*.png -type f))
+	cd ../$IMAGE_DIR_OUT
+	else
 	echo "compose images : $png_subdirs..."
 	recolor_paths=$COMPOSITE_PATHS
 	composite_paths=($(make_paths $png_subdirs))
@@ -223,15 +250,6 @@ then
 	part1_paths=($(make_paths $png_subdirs))
 	recolor_paths=$PART2_PATHS
 	part2_paths=($(make_paths $png_subdirs))
-	else
-	#elif [[ $path_lists == 'dir' ]]
-	echo "compose images : $image_dir..."
-	part1_paths=($(find **/*.png -type f))
-	composite_paths=$image_dir
-	part2_dir=stock
-	cd $part2_dir
-	part2_paths=($(find **/*.png -type f))
-	cd ../$image_dir
 	fi
 	compose_images $composite_paths
 fi
