5724afd867b1ddecdc5e7f0b4a2d09a2daa314ed
cedric <cedl38@gmail.com>
2013-04-03 22:55:06 +0200

    update makepkg.sh

--- makepkg.sh
+++ makepkg.sh
@@ -3,9 +3,9 @@
 # use -f to force update
 
 source cache/PKGBUILD
-git archive --format=tar.gz --prefix=$pkgname-junk/ HEAD > cache/$pkgname-junk.tar.gz
+git archive --format=tar.gz --prefix=$pkgname-$pkgver/ HEAD > cache/$pkgname-$pkgver.tar.gz
 
 cd cache
 makepkg $1 --source --skipchecksums
 makepkg $1 --skipchecksums
-yaourt -S $pkgname-junk
+yaourt -U $pkgname-junk-*tar.xz
